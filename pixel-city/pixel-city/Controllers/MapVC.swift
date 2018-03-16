3ff54a8224210bdf997722074eada88c25196cd7
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-16 15:11:03 +0800

    Building user interfae.

--- pixel-city/pixel-city/Controllers/MapVC.swift
+++ pixel-city/pixel-city/Controllers/MapVC.swift
@@ -1,9 +1,21 @@
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
     }
+
+    @IBAction func centerMapButttonWasPressed(_ sender: Any) {
+    }
+
 }
 
+extension MapVC: MKMapViewDelegate {
+
+}
